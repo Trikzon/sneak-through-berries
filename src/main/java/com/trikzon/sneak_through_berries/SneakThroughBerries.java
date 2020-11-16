44477c353b5b2c8dbadf100f49a594451b68c370
Trikzon <trikzondev@gmail.com>
2020-11-16 09:54:08 -0500

    1.2.0+1.16.4-forge

--- src/main/java/com/trikzon/sneak_through_berries/SneakThroughBerries.java
+++ src/main/java/com/trikzon/sneak_through_berries/SneakThroughBerries.java
@@ -9,18 +9,60 @@
  */
 package com.trikzon.sneak_through_berries;
 
-import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class SneakThroughBerries implements ModInitializer {
+@Mod(SneakThroughBerries.MOD_ID)
+public class SneakThroughBerries {
     public static final String MOD_ID = "sneak-through-berries";
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
     public static Config.ConfigBean CONFIG;
 
-    @Override
-    public void onInitialize() {
+    public SneakThroughBerries() {
         CONFIG = Config.init(LOGGER, MOD_ID);
+
+        MinecraftForge.EVENT_BUS.addListener(this::onLivingAttack);
+    }
+
+    private void onLivingAttack(LivingAttackEvent event) {
+        if (!(event.getEntity() instanceof PlayerEntity)) return;
+        PlayerEntity player = (PlayerEntity) event.getEntity();
+
+        if (CONFIG.sneakToStopDamage && player.isSneaking()) {
+            event.setCanceled(true);
+            return;
+        }
+
+        ItemStack feet = player.getItemStackFromSlot(EquipmentSlotType.FEET);
+        ItemStack legs = player.getItemStackFromSlot(EquipmentSlotType.LEGS);
+        ItemStack chest = player.getItemStackFromSlot(EquipmentSlotType.CHEST);
+        ItemStack head = player.getItemStackFromSlot(EquipmentSlotType.HEAD);
+
+        if (CONFIG.requiredToWalk.boots && feet.isEmpty()) return;
+        if (CONFIG.requiredToWalk.leggings && legs.isEmpty()) return;
+        if (CONFIG.requiredToWalk.chestPlate && chest.isEmpty()) return;
+        if (CONFIG.requiredToWalk.helmet && head.isEmpty()) return;
+
+        event.setCanceled(true);
+
+        World world = event.getEntity().world;
+        if (world.isRemote) return;
+        if (!CONFIG.requiredArmorLosesDurabilityWhenWalking) return;
+        if (world.rand.nextInt(20) != 0) return;
+
+        ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
+        if (CONFIG.requiredToWalk.boots) feet.attemptDamageItem(1, world.rand, serverPlayer);
+        if (CONFIG.requiredToWalk.leggings) legs.attemptDamageItem(1, world.rand, serverPlayer);
+        if (CONFIG.requiredToWalk.chestPlate) chest.attemptDamageItem(1, world.rand, serverPlayer);
+        if (CONFIG.requiredToWalk.helmet) head.attemptDamageItem(1, world.rand, serverPlayer);
     }
 }
